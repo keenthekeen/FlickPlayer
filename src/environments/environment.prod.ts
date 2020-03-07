78220b1a0115bf40360918d878b3e2db8f430d4b
Siwat Techavoranant <keen@keendev.net>
2020-03-07 20:50:45 +0700

    Install Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyChVnSa_0Lk0U4SvPi_yTmpzCC5W2CZqaI',
+    authDomain: 'keendev-flick-a.firebaseapp.com',
+    databaseURL: 'https://keendev-flick-a.firebaseio.com',
+    projectId: 'keendev-flick-a',
+    storageBucket: 'keendev-flick-a.appspot.com',
+    messagingSenderId: '1075695455520',
+    appId: '1:1075695455520:web:43435b3067a0c1b0ba9b45',
+    measurementId: 'G-NRVJNXG64Q'
+  }
 };
+
+export const ManEndpoint = 'https://flick-man.docchula.com/v1/';
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,20 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyChVnSa_0Lk0U4SvPi_yTmpzCC5W2CZqaI',
+    authDomain: 'keendev-flick-a.firebaseapp.com',
+    databaseURL: 'https://keendev-flick-a.firebaseio.com',
+    projectId: 'keendev-flick-a',
+    storageBucket: 'keendev-flick-a.appspot.com',
+    messagingSenderId: '1075695455520',
+    appId: '1:1075695455520:web:43435b3067a0c1b0ba9b45',
+    measurementId: 'G-NRVJNXG64Q'
+  }
 };
+
+export const ManEndpoint = 'https://flick-man.docchula.com/v1/';
 
 /*
  * For easier debugging in development mode, you can import the following file
